e207b4b109526fa2aaa1f0fe903a5ba5dd0b37fc
Brett Duncavage <brett.duncavage@rd.io>
2015-01-22 18:29:18 -0800

    Basic functionality
    Creates the pixelated version of the album art and maps the cell colors to an album in the collection based
    on the album's dominant color.
    
    This code is not pretty, I am not a javascript programmer.

--- main.js
+++ main.js
@@ -12,6 +12,8 @@
 
       this.currentAlbums = [];
       this.possibleAlbums = [];
+      this.colorToAlbum = {};
+      this.processingComplete = false;
 
       if (!rdioUtils.startupChecks()) {
         return;
@@ -48,14 +50,14 @@
       $('.albums').empty();
       this.currentAlbums = [];
 
-      var addAlbum = function(album) {
+      var addAlbum = function(album, alternativeColor) {
         if (!album.canStream) {
           return;
         }
 
         self.currentAlbums.push(album);
 
-        var widget = rdioUtils.albumWidget(album);
+        var widget = rdioUtils.albumWidget(album, alternativeColor);
         var $widget = $(widget.element());
         $('.albums').append($widget);
 
@@ -70,11 +72,119 @@
           .appendTo($widget.find('.rdio-utils-album-hover-overlay'));
       };
 
-      while (this.currentAlbums.length < 20 && this.indicies.length) {
-        var index = this.indicies.pop();
-        var album = this.possibleAlbums[index];
-        addAlbum(album);
+      var randomAlbumIndex = Math.floor( Math.random() * this.possibleAlbums.length);
+      var randomAlbum = this.possibleAlbums[randomAlbumIndex];
+
+      $('#largeAlbum').bind('load', function() {
+       // var staticCanvas = document.createElement('staticCanvas');
+        var staticCanvas = document.getElementById('staticCanvas');
+        var drawingCanvas = document.getElementById('drawingCanvas');
+       // $('.gridContainer').appendChild(staticCanvas);
+        var staticContext = staticCanvas.getContext("2d");
+        staticContext.drawImage(document.getElementById("largeAlbum"), 0, 0);
+        self.drawingContext = drawingCanvas.getContext("2d");
+        // Extract each cell from the image, draw it into a separate staticCanvas and analyze it.
+        var extractedColors = [];
+        var cf = new ColorThief();
+
+        var gridSizeInPx = 600;
+        var minBlockSize = 20;
+        var startBlockSize = gridSizeInPx / 2;
+        var blockSize = startBlockSize;
+        var gridSize = gridSizeInPx / blockSize;
+
+        function processBlock(col, row, computeAlbums) {
+          var blockData = staticContext.getImageData(col * blockSize, row * blockSize, blockSize, blockSize);
+          // Analyze the block data to get a color.
+          var color = cf.getColorFromImageData(blockData);
+          extractedColors.push(color);
+          self.drawingContext.fillStyle = 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')';
+          self.drawingContext.fillRect(col * blockSize, row * blockSize, blockSize, blockSize);
+          if (computeAlbums) {
+            // try to find the album for this color
+            var currentDistance = -1;
+            var candidateAlbum;
+            for (var i = 0; i < self.possibleAlbums.length; i++) {
+              var color1 = self.possibleAlbums[i].dominantColor;
+              var color2 = color;
+
+              var dist = Math.sqrt(Math.pow(color1.r - color2[0],2) + Math.pow(color1.g - color2[1],2) + Math.pow(color1.b - color2[2],2));
+              if (dist < currentDistance || currentDistance == -1) {
+                candidateAlbum = self.possibleAlbums[i];
+                currentDistance = dist;
+              }
+            }
+            self.colorToAlbum[""+color2[0]+color[1]+color[2]] = candidateAlbum;
+          }
+        }
+
+        var currentRow = 0;
+        var currentCol = 0;
+        var computeAlbums = false;
+
+        function moveToNextBlock() {
+
+          if (currentRow >= gridSize) {
+            if (blockSize > minBlockSize ) {
+              // reduce block size
+              blockSize -= 40;
+              gridSize = Math.floor(gridSizeInPx / blockSize);
+              currentCol = 0;
+              currentRow = 0;
+              computeAlbums = blockSize <= minBlockSize;
+              moveToNextBlock();
+            } else {
+              self.processingComplete = true;
+              return;
+            }
+          }
+          if (currentCol < gridSize) {
+            processBlock(currentCol, currentRow, computeAlbums);
+            currentCol++;
+            if (currentCol >= gridSize) {
+              currentCol = 0;
+              currentRow++
+            }
+          }
+          setTimeout(moveToNextBlock, 5);
+        }
+        setTimeout(moveToNextBlock, 2000);
+      });
+
+      function findPos(obj) {
+        var curleft = 0, curtop = 0;
+        if (obj.offsetParent) {
+          do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+          } while (obj = obj.offsetParent);
+          return { x: curleft, y: curtop };
+        }
+        return undefined;
       }
+
+      $('#largeAlbum').attr('src', randomAlbum.bigIcon);
+
+      $('body').css('background', 'url("' + randomAlbum.backgroundImageUrl + '&w=1200") no-repeat center center fixed');
+      $('body').css('background-size', 'cover');
+
+      $('#drawingCanvas').click(function(e) {
+        if (!self.processingComplete) {
+          return;
+        }
+        var pos = findPos(this);
+        var x = e.pageX - pos.x;
+        var y = e.pageY - pos.y;
+
+        var color = self.drawingContext.getImageData(x, y, 1, 1).data;
+        var key = ""+color[0]+color[1]+color[2];
+        var album = self.colorToAlbum[key];
+
+        $('#chosenColor').css('background-color', 'rgb('+color[0]+','+color[1]+','+color[2]+')');
+        var ac = album.dominantColor;
+        $('#destinationAlbumColor').css('background-color', 'rgb('+ac.r+','+ac.g+','+ac.b+')');
+        $('#destinationAlbumIcon').attr('src', album.icon);
+      });
     },
 
     // ----------
